253e3f23b5e8eeda1e8bc599b9f559df9d93592d
Peter Billam <contact.html@pjb.com.au>
2010-08-19 01:24:57 +0000

    Import of PJB/Math-WalshTransform-1.17 from CPAN.
    gitpan-cpan-distribution: Math-WalshTransform
    gitpan-cpan-version:      1.17
    gitpan-cpan-path:         PJB/Math-WalshTransform-1.17.tar.gz
    gitpan-cpan-author:       PJB
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -7,7 +7,7 @@
 #            modify it under the same terms as Perl itself.             #
 #########################################################################
 
-use Test::Simple tests => 12;
+use Test::Simple tests => 16;
 use Math::WalshTransform qw(:ALL);
 # $Math::WalshTransform::PP = 1;
 my $eps = .000000001;
@@ -17,6 +17,10 @@ my @X = &fht(@x);
 ok (&equal(\@X,[1,0,0,-1]), "Hadamard transform");
 @X = &fwt(@x);
 ok (&equal(\@X,[1,0,-1,0]), "Walsh transform");
+my @IHT = &fhtinv(1,0,0,-1);
+ok (&equal(\@IHT, [0,2,2,0]), "Inverse Hadamard transform");
+my @IWT = &fwtinv(1,0,-1,0);
+ok (&equal(\@IWT, [0,2,2,0]), "Inverse Walsh transform");
 
 my @f;
 for ($n=0; $n<=1023; $n++) { push @f, rand 9.9; }
@@ -37,6 +41,7 @@ my @f1; for ($n=0; $n<=511; $n++) { push @f1, rand 9.9; }
 my @f2; for ($n=0; $n<=511; $n++) { push @f2, rand 9.9; }
 my @lc1 = &Math::WalshTransform::old_logical_convolution(\@f1, \@f2);
 my @lc2 = &logical_convolution(\@f1, \@f2);
+# print "f1= @f1\nf2= @f2\nlc2= @lc2\n"; # to feed hard values to the lua test
 ok (&equal(\@lc1,\@lc2), "Logical Convolution");
 
 ok (abs(2.5 - &size(1.0,-0.5,2.0,-1.0)) < $eps, "size");
@@ -52,6 +57,13 @@ ok (&equal(\@y, [0.4,-0.2,0.8,-0.4]),"normalise");
 
 @y = &average([0.5,-0.5,3.0,1.0],[-0.5,0.0,1.0,2.0],[0.6,1.4,0.-2.2,0.3]);
 ok (&equal(\@y,[0.2,0.3,0.6,1.1]), "average");
+
+@y = &biggest(3,  0.5,-0.5,3.0,1.0,-0.5,0.0,1.0,2.0,0.6,1.4,0,-2.2,0.3);
+ok (&equal(\@y,[0, 0, 3, 0, 0, 0, 0, 2, 0, 0, 0, -2.2, 0]), "biggest");
+
+@y = &power_spectrum(.5,-.5,3,1,-.5,0,1,2,.6,1.4,0,-2.2,-.7,4.2,-1,5);
+my @y2 = (0.74390625, 0.0025, 0.330625, 0.15015625, 0.06890625, 0.105625, 0.49, 0.05640625, 0.0225, 0.00765625, 0.02640625, 0.1225, 1.1025, 0.23765625, 0.47265625, 0.25);
+ok (&equal(\@y,\@y2), "power_spectrum");
 
 # --------------------------- infrastructure ----------------
 sub equal { my ($xref, $yref) = @_;
